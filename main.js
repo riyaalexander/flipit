d117eeee0f5cb671f5962fd2dbe1220e09bcb0ce
riyaalexander <riyaalexander@Riyas-Air.fios-router.home>
2023-01-16 22:54:16 -0500

    It doesn't want to run. Trial 2

--- main.js
+++ main.js
@@ -1,36 +1,53 @@
-let cards = [];
+let cards = []
 let firstCard = null;
 let count = 0;
 let score = 0;
 let matchCards = 0;
-let matches = 8
-let points = 10
+let matches = 8;
+let points = 10;
 
 
 // shuffle the deck and get the first 16 cards
-fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-    .then(response => response.json())
-    .then(data => {
+fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+    .then((response) => response.json())
+    .then((data) => {
         let deckId = data.deck_id;
-        fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=16`)
-            .then(response => response.json())
-            .then(data => {
+        fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=8`)
+            .then((response) => response.json())
+            .then((data) => {
                 cards = data.cards;
+            // pull 8 cards from the deck
+                let pullCards = cards.slice(0, 8);
+                // clone the cards array
+                let duplicateCards = [...pullCards];
+                // concatenate the two arrays
+                pullCards = pullCards.concat(duplicateCards);
+                // shuffle the cards
+                function shuffle(array) {
+                for (let i = array.length - 1; i > 0; i--) {
+                    let j = Math.floor(Math.random() * (i + 1));
+                        [array[i], array[j]] = [array[j], array[i]];
+                 }
+                }
+                shuffle(pullCards);
+
+
+
                 // create the card elements and add them to the page
-                let gameContainer = document.getElementById('game-container');
-                let row1 = document.getElementById('row1');
-                let row2 = document.getElementById('row2');
-                let row3 = document.getElementById('row3');
-                let row4 = document.getElementById('row4');
+                let gameContainer = document.getElementById("game-container");
+                let row1 = document.getElementById("row1");
+                let row2 = document.getElementById("row2");
+                let row3 = document.getElementById("row3");
+                let row4 = document.getElementById("row4");
                 for (let i = 0; i < cards.length; i++) {
                     let card = cards[i];
-                    let cardElement = document.createElement('div');
-                    cardElement.classList.add('card');
+                    let cardElement = document.createElement("div");
+                    cardElement.classList.add("card");
                     cardElement.innerHTML = `
                         <div class="card-back"></div>
                         <div class="card-front" style="background-image: url(${card.image})"></div>
                     `;
-                    cardElement.addEventListener('click', flipCard);
+                    cardElement.addEventListener("click", flipCard);
                     // game container rows
                     if (i % 4 === 0) {
                         row1.appendChild(cardElement);
@@ -42,9 +59,9 @@ fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
                         row4.appendChild(cardElement);
                     }
                 }
-                // start the timer
-                // startTimer();
+                
 
+        
                 // start the timer for 3 minutes
                 let timer = document.getElementById("timer");
                 let countDownDate = new Date().getTime() + 180000;
@@ -60,7 +77,7 @@ fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
                             }
                         })
                 // start the cards face down
-                let cardElements = document.querySelectorAll('.card');
+                let cardElements = document.querySelectorAll(".card");
                 for (let i = 0; i < cardElements.length; i++) {
                     cardElements[i].classList.add("flip");
                 }
@@ -70,7 +87,7 @@ fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
 // function to flip the card and check for a match
 function flipCard(event) {
     let card = event.currentTarget;
-    card.classList.toggle('flip');
+    card.classList.toggle("flip");
     // remove the cards if they match
     if (firstCard === card) {
         matchCards++;
@@ -83,17 +100,17 @@ function flipCard(event) {
     if (!firstCard) {
         firstCard = card;
     } else {
-        let firstCardFront = firstCard.querySelector('.card-front');
-        let secondCardFront = card.querySelector('.card-front');
+        let firstCardFront = firstCard.querySelector(".card-front");
+        let secondCardFront = card.querySelector(".card-front");
         if (firstCardFront.style.backgroundImage === secondCardFront.style.backgroundImage) {
             firstCard = null;
         } else {
             setTimeout(() => {
-                firstCard.classList.remove('flip');
-                card.classList.remove('flip');
+                firstCard.classList.remove("flip");
+                card.classList.remove("flip");
                 firstCard = null;
             }, 1000);
         }
-    } // end of if else
+    } 
 }// end of function
 
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Flip It
+Stimulate your mind with this memory game!
+
+This game will:
